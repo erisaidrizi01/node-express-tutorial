cdf5e7b4e78e9278c8174848dff6a94adb90fcf2
Erisa Idrizi <erisa@onlinenow.eu>
2023-11-02 12:29:53 +0100

    First commit

---
+++ 06-jobs-api/starter/controllers/auth.js
@@ -0,0 +1,31 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+// const bcrypt = require('bcryptjs')
+
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    // const salt = await bcrypt.genSalt(10)
+    // const hashedPassword = await bcrypt.hash(password, salt)
+
+    // const tempUser = { name, email, password: hashedPassword}
+
+    // const user = await User.create({ ...tempUser })
+    // res.status(StatusCodes.CREATED).json({ user })
+
+    const user = await User.create({ ...req.body })
+    const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+        expiresIn:'30d'
+    })
+    res.status(StatusCodes.CREATED).json({ token })
+}
+
+const login = async (req, res) => {
+    res.send('login user')
+}
+
+module.exports = {
+    register,
+    login
+}
